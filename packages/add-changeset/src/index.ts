c0d2db54c38c3245880021274a08fd406e0096ff
Geoff Lamrock <geoff.lamrock@gmail.com>
2022-04-28 16:40:03 +1000

    Adds changeset action

---
+++ packages/add-changeset/src/index.ts
@@ -0,0 +1,90 @@
+import { Changeset, Release, VersionType } from "@changesets/types";
+import { getInput, setOutput, setFailed, getMultilineInput } from "@actions/core";
+import glob, { IOptions } from "glob";
+import fs from "fs";
+import write from "@changesets/write";
+
+type AddChangesetParameters = {
+    filter?: RegExp;
+    ignore?: Array<string>;
+    type?: VersionType;
+    summary: string;
+    cwd?: string;
+};
+
+const globAsync = (pattern: string, options: IOptions): Promise<string[]> => {
+    return new Promise<string[]>((resolve, reject) => {
+        glob(pattern, options, (error, matches) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(matches);
+            }
+        });
+    });
+};
+
+type PackageJson = {
+    name: string;
+};
+
+const loadProjectPackageJson = async (packageJsonPath: string): Promise<PackageJson> => {
+    if (!fs.existsSync(packageJsonPath)) {
+        throw new Error(`Could not locate package.json in ${packageJsonPath}`);
+    }
+
+    return await import(packageJsonPath);
+};
+
+async function addChangeset({ filter, ignore, type, summary, cwd }: AddChangesetParameters): Promise<string> {
+    const packageJsonFilePaths = await globAsync("**/package.json", { absolute: true, ignore: ignore });
+
+    const releases: Release[] = [];
+
+    for (const packageJsonPath of packageJsonFilePaths) {
+        const packageJson = await loadProjectPackageJson(packageJsonPath);
+
+        let addPackage = true;
+
+        if (filter !== undefined && !filter.test(packageJson.name)) {
+            addPackage = false;
+        }
+
+        if (addPackage) {
+            releases.push({
+                name: packageJson.name,
+                type: type || "patch",
+            });
+        }
+    }
+
+    const changeset: Changeset = {
+        summary,
+        releases,
+    };
+
+    return write(changeset, cwd || ".");
+}
+
+try {
+    const filter = getInput("filter");
+    const ignore = getMultilineInput("ignore");
+    const type = getInput("type");
+
+    if (type !== "major" && type !== "minor" && type !== "patch") throw new Error("Type must be one of 'major', 'minor' or 'patch'");
+
+    const summary = getInput("summary");
+    const cwd = getInput("cwd");
+
+    const changesetFilePath = addChangeset({
+        filter: filter === "" ? undefined : new RegExp(filter),
+        summary,
+        ignore,
+        type,
+        cwd,
+    });
+
+    setOutput("changeset", changesetFilePath);
+} catch (error) {
+    setFailed(error.message);
+}
