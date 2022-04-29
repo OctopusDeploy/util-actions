9128d0c85409e0d7ffcb7a280b96cb93c7c238a8
Geoff Lamrock <geoff.lamrock@gmail.com>
2022-04-29 16:26:23 +1000

    Adds tests and refactoring

--- packages/add-changeset/src/index.ts
+++ packages/add-changeset/src/index.ts
@@ -1,31 +1,4 @@
-import { getInput, setOutput, setFailed, getMultilineInput } from "@actions/core";
-import { addChangeset } from "./addChangeset";
+import { GitHubActionsContextImpl } from "@octopusdeploy/shared-action-utils";
+import { addChangesetAction } from "./addChangesetAction";
 
-// todo: convert to use context when this is available
-async function addChangesetAction() {
-    try {
-        const filter = getInput("filter");
-        const ignore = getMultilineInput("ignore");
-        const type = getInput("type");
-
-        if (type !== "major" && type !== "minor" && type !== "patch") throw new Error("Type must be one of 'major', 'minor' or 'patch'");
-
-        const summary = getInput("summary");
-        const cwd = getInput("cwd");
-
-        const { name, path } = await addChangeset({
-            filter: filter === "" ? undefined : new RegExp(filter),
-            summary,
-            ignore,
-            type,
-            cwd,
-        });
-
-        setOutput("changesetName", name);
-        setOutput("changesetPath", path);
-    } catch (error) {
-        setFailed(error.message);
-    }
-}
-
-addChangesetAction().catch(() => process.exit(1));
+addChangesetAction(new GitHubActionsContextImpl()).catch(() => process.exit(1));
