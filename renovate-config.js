ae68d423965420e6fef560be9e6baa4b1f21484c
lourens-octopus <165963988+lourens-octopus@users.noreply.github.com>
2026-05-01 09:09:27 +1200

    fix(renovate): Update renovate to install pnpm (#288)

--- renovate-config.js
+++ renovate-config.js
@@ -22,19 +22,25 @@ module.exports = {
     repositories: ["OctopusDeploy/util-actions"],
     packageRules: [
         {
-        matchDatasources: ['npm'],
-        minimumReleaseAge: '2 days'
+            matchDatasources: ['npm'],
+            minimumReleaseAge: '2 days'
+        },
+        {
+            // Ignore workspace packages that aren't published to npm
+            matchPackageNames: ['@octopusdeploy/shared-action-utils'],
+            enabled: false
         }
     ],
     timezone: "Australia/Brisbane",
     onboarding: false,
     requireConfig: false,
+    binarySource: "install",
     constraints: {
         pnpm: "8.15.9",
     },
     allowedPostUpgradeCommands: [".*"],
     postUpgradeTasks: {
-        commands: ["npm i -g pnpm@8.15.9", "pnpm install", "pnpm build", "rm -rf node_modules packages/*/node_modules"],
+        commands: ["pnpm install", "pnpm build", "rm -rf node_modules packages/*/node_modules"],
         fileFilters: ["**/index.js"],
         executionMode: "update",
     },
